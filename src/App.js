b3449f807534368e2ff54cb0932703290883cfdb
elisa <bascongarcia@gmail.com>
2022-08-19 20:36:20 +0200

    Iteration 0 Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+    <h1>Holita</h1>
+    <iframe src="https://giphy.com/embed/lXiRG1vwLewnehlxS" width="357" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/super-bowl-shark-i-have-no-idea-what-im-doing-lXiRG1vwLewnehlxS"></a></p>
+  </div>;
 }
-
 export default App;
